3f0d68826d0263dd7195c8e96749c0764359c88c
Vipulkem <singhvipul169@gmail.com>
2025-10-07 14:08:42 +0530

    commit All

---
+++ Day1.R
@@ -0,0 +1,15 @@
+print(7+3+5)
+
+# this is the inline comment
+
+x = 9
+y = 99
+x+y  # ctrl+r to run this code
+
+
+demo <- "Hello how are you"
+print(demo)
+
+
+help.search("data input")   # for search in r language  
+
