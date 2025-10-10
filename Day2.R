93cb13bf3aeacef684e4eb667a449d4c66ce09d9
Vipulkem <singhvipul169@gmail.com>
2025-10-10 14:07:16 +0530

    add success

---
+++ Day2.R
@@ -0,0 +1,16 @@
+sum1 <- 66+99; sum2 <- 19+89  # we can assign multiple variable in single line using ;
+print(sum1)   # ctrl+enter to run the program
+print(sum2)
+
+x <- 1:10  # 1:10 = Range 1 to 10
+plot(x)
+
+?plot(x)  # find or search about the plot 
+
+
+# R is a case sensitive language
+
+a <- 10
+A <- 20
+print(a)
+print(A)
