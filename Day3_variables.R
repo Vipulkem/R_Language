7652c4257001291cef9a1d5a50e55e9707c0fe73
Vipulkem <singhvipul169@gmail.com>
2025-10-10 14:35:48 +0530

    file add

---
+++ Day3_variables.R
@@ -0,0 +1,36 @@
+# Variables in R Language
+
+# contain = {number, characer, .(Dot), _(Underscore) }
+
+# start with = {character,.(Dot)}
+#note -> in R language variable name can not start with _(underscore).
+
+# Assignment of variable in R Language
+var1 <- 20
+print(var1)
+
+var2 = 34
+print(var2)
+
+56 -> var3
+print(var3)
+
+# print multiple varable at same line or using single *cat* function
+cat(var1,"  ",var2)
+
+cat(var2,"\n",var3)
+
+# assigning cariable values
+
+var_name <- 65; print(var_name)
+
+var5_name <- "name"; print(var5_name)
+
+_var <- 67  # not allowed in R language
+
+.varName <- 99; print(.varName)
+# variable name start with . allowed in R language
+
+var* <- "This is not allowed in R Language"
+
+5varName <- 8969 # Variable name start with number not allowed in R language
